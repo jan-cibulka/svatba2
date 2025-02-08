dd929f5a1ddfb5cdf2fbfaea8d07f196926fe2d8
Jan Cibulka <jan.cibulka@ui.com>
2025-02-08 16:35:10 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,26 @@ const colorText1 = "#2b2b2b";
 const colorText2 = "#4a4a4a";
 
 const arrowColor = "#000";
-const dividerColor = "#ddd";
+const dividerColor = "#bbb";
 
 const backgroundColor1 = "#edece8";
 const backgroundColor2 = "#d2cfcb";
 
+const sizeText1 = "20px";
+const sizeText2 = "16px";
+
+const sizeDivider = "3px";
+
 const variablesStyle = `
---text-color-1: ${colorText1};
---text-color-2: ${colorText2};
---arrow-color: ${arrowColor};
---divider-color-1: ${dividerColor};
---background-color-1: ${backgroundColor1};
---background-color-2: ${backgroundColor2};
+--color-text-1: ${colorText1};
+--color-text-2: ${colorText2};
+--color-arrow: ${arrowColor};
+--color-divider-1: ${dividerColor};
+--color-background-1: ${backgroundColor1};
+--color-background-2: ${backgroundColor2};
+--size-text-1: ${sizeText1};
+--size-text-2: ${sizeText2};
+--size-divider: ${sizeDivider};
 
 `;
 
--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -1,7 +1,40 @@
 import { FC } from "react";
 
+import "./Section1.css";
+
+const title = "DEAR FRIENDS AND FAMILY";
+const text1 =
+  "This summer a very special and happy event is going to happen - our wedding";
+
+const text2 =
+  "It is impossible to imagine this day without our closest people. Thus, we ae happy to invite you to join and share this wonderful occasion with us";
+
+const date = "29.08.23";
+const place = "Krimice";
 const Section1: FC = () => {
-  return <div className="section-wrapper">Section1</div>;
+  return (
+    <div className="section-wrapper section1">
+      <div className="names">Barca & Zdenda</div>
+      <div className="date-place">
+        <div className="container">
+          <div className="item-top">{date}</div>
+          <div className="item-bottom">date</div>
+        </div>
+        <div className="container">
+          <div className="item-top">{place}</div>
+          <div className="item-bottom">place</div>
+        </div>
+      </div>
+      <div className="image-wrapper">
+        <img src="/src/assets/xaxa.jpg" alt="xaxa" width={100} height={100} />
+      </div>
+      <div className="text">
+        <div className="title">{title}</div>
+        <div className="text-1">{text1}</div>
+        <div className="text-2">{text2}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Section1;
