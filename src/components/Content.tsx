12a746f8bc2aef31b1703857e07257bc3d68a156
Jan Cibulka <jan.cibulka@ui.com>
2025-02-11 21:52:01 +0100

    fix

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,12 +2,19 @@ import { FC } from "react";
 
 import "./Content.css";
 import Section1 from "./Section1";
+import Map from "./Map";
 const Content: FC = () => {
   return (
-    <div className="content-wrapper">
-      <div className="background"></div>
-      <Section1 />
-    </div>
+    <>
+      <div className="content-wrapper">
+        <div className="background"></div>
+        <Section1 />
+      </div>
+      <div className="content-wrapper dark">
+        <div className="background"></div>
+        <Map />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Map.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+import "./Map.css";
+
+const Map: FC = () => {
+  return (
+    <div className="section-wrapper map-wrapper">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2077.229199696126!2d13.31328149210469!3d49.75813441150119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470af3b00b9b32c5%3A0xf21128a3c14f272d!2sRestaurace%20Sokolovna%20K%C5%99imice!5e0!3m2!1scs!2scz!4v1739305871885!5m2!1scs!2scz"
+        width="60%"
+        height="450"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Map;
