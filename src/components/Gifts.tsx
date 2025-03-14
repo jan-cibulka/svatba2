70f09b753dc3b0b2e8af2a171ba0f4e2b7ad55ba
zdenek.castoral <zdenek.castoral@inventi.cz>
2025-03-14 15:18:59 +0100

    Gifts.

--- src/components/Gifts.tsx
+++ src/components/Gifts.tsx
@@ -4,10 +4,10 @@ import "./Gifts.css";
 const Gifts: FC = () => {
   return (
     <div className="section-wrapper">
-      <div className="title">Svatební Dary</div>
+      <div className="title">Svatební dary</div>
       <p className="gifts-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+        Se svatebními dary si nelámte hlavu, nejdůležitější pro nás je, že dorazíte a strávíte s námi náš den. Pokud nás přece jen budete chtít nějak obdarovat,
+        budeme rádi za příspěvek do začátku naší společné budoucnosti.
       </p>
       <div className="bank-info">
         <div className="account-details">
